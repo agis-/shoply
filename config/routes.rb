1f741ba81e9a9c24d616d28dfdcc3d6d3494208a
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-22 18:55:22 +0300

    polish

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Deals::Application.routes.draw do
   root to: 'Cities#show'
 
-  get '/welcome'        =>      'Welcome#home', :as => :welcome
-  post '/welcome' => 'Cities#welcome'
+  get   '/welcome'        =>      'Welcome#home'
+  post  '/welcome'        =>      'Cities#welcome'
   match '/admin'          =>      'Admin::Pages#index'
 
   resources :cities,      only: :show

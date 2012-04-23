0a42fc67493f51ba46d7c3bed78dd13476240f67
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-23 16:28:06 +0300

    make immune to SQL-injection

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,11 +2,9 @@ class VendorsController < ApplicationController
   before_filter :redirect_if_no_cookie
 
   def show
-    category = params[:c]
     @vendor = Vendor.find(params[:id])
-
-    if category
-      @offers = @vendor.offers.active.where("category_id = #{category}").page(params[:page])
+    if params[:c]
+      @offers = @vendor.offers.active.where("category_id = ?", params[:c]).page(params[:page])
     else
       @offers = @vendor.offers.active.page(params[:page])
     end

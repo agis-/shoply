050e4e2ac26bad73544b1b7d711432ed1de15953
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-28 20:00:51 +0300

    create draft spec for Admin::PagesController

---
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -0,0 +1,23 @@
+# require 'spec_helper'
+
+# describe Admin::PagesController do
+#   context "normal user" do
+#     it "should redirect to login" do
+#       get :index
+#       response.should redirect_to '/admin/login'
+#     end
+#   end
+
+#   context "Vendor user" do
+#     before do
+#       visit '/admin/login'
+      
+
+#     end
+
+#     it "should take you in" do
+#       get :index
+#       response.should be_success
+#     end
+#   end
+# end
